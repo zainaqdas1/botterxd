01ad7de4c8c8bb14b01ff084c871e578155b5d9d
zainaqdas1 <lovershostbuddy@gmail.com>
2017-12-15 17:26:38 +0500

    Add files via upload

--- react.php
+++ react.php
@@ -1,37 +1,24 @@
 <?php
-// Script Created By Syed Shahab
-// to use this script just attach it with the simple bot script and use the iphone on that script it will get tokens from cokis
-$directory=opendir('XD');
-$file_array=array();
-while($file=readdir($directory))
-{
-	if($file!=='.' && $file!=='..')
-	{
-		$file_array[]=$file;
-	}
+date_default_timezone_set('Asia/Jakarta');
+$yx=opendir('myToken'); while($isi=readdir($yx)){ if($isi != '.' && $isi != '..'){ $token=$isi;
+$stat= json_decode(auto('https://graph.facebook.com/me/home?fields=id,from,comments&limit=10&access_token='.$token),true);
+for($i=1;$i<=count($stat[data]);$i++){ $x=$stat[data][$i-1][id].'~'; $y= fopen('komen.txt','a'); fwrite($y,$x); fclose($y);
+$nom = $stat[data][$i-1][comments][count]+1;
+$nom = $stat[data][$i-1][comments][count]+1;
+$name= json_decode(auto('https://graph.facebook.com/'.$stat[data][$i-1][from][id].'?fields=name&access_token='.$token),true);
+$nam=$name[name]; $ama= explode(' ',$nam); $nama=$ama[0]; $mr=''.$nama;
+$kata= array('cinta.php','motivasi.php','motto.php','bijak.php',); $kata=$kata[rand(0,count($kata)-1)]; $katakata= auto('http://ciambar.us.to/'.$kata);
+$jam= array('01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','00',);
+$sapa=array('',);
+$ucapan = gmdate('H',time()+7*3600); $ucapan = str_replace($jam,$sapa,$ucapan);
+$pesan= array('',);
+$pesan=$pesan[rand(0,count($pesan)-1)];
+$message=$pesan;
+auto('');
+auto('https://graph.facebook.com/'.$stat[data][$i-1][id].'/reactions?type=LOVE&method=POST&access_token='.$token.'');
 }
-for($i=0;$i<sizeof($file_array);$i++)
-{
-	$access_token= file_get_contents('XD/'.$file_array[$i]);
-	$token=explode('*',$access_token);
-	    $limitnf=05; 
-        $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token[0].'&offset=0&limit='.$limitnf.''),true);
-		echo $puaru[data];
-		echo "<br/>";
-        for($d=1;$d<=count($puaru[data]);$d++){
-        set_time_limit(0);
-        $camxuc= array('LOVE', 'THANKFUL');
-        $mess=$camxuc[rand(0,count($camxuc)-1)];
-        echo puaru('https://graph.facebook.com/'.$puaru[data][$d-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token[0].'');       
-}	    
 }
-echo "Creat By Chaudhrys Botter";
-function puaru($url){
-$data = curl_init();
-curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($data, CURLOPT_URL, $url);
-$hasil = curl_exec($data);
-curl_close($data);
-return $hasil;
-} 
+}
+function auto($url){ $data = curl_init(); curl_setopt($data,CURLOPT_RETURNTRANSFER,1); curl_setopt($data,CURLOPT_URL, $url); $hasil = curl_exec($data); curl_close($data); return $hasil;
+}
 ?>
